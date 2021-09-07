6649e71a2ddb83a13d0d37fbbcffd7fbf9bff833
hotsoup42 <jose22gw@gmail.com>
2021-09-07 13:35:12 -0700

    query for 'view all employees' is working

--- index.js
+++ index.js
@@ -1,70 +1,75 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require('console.table');
 const db = mysql.createConnection(
   {
-    host: 'localhost',
+    host: "localhost",
     // MySQL username,
-    user: 'root',
+    user: "root",
     // MySQL password
-    password: '',
-    database: 'classlist_db'
+    password: "",
+    database: "employee_db",
   },
-  console.log(`Connected to the classlist_db database.`)
+  console.log(`Connected to the employee_db database.`)
 );
 
-function whatToDoQestion(){
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'What would you like to do? ',
-      name: 'whatToDoList',
-      choices: ['View All Employees', 'Add Employee','Update Employee Role', 'View All Roles', 'Add Role','View All Departments', 'Add Department'],
-    },
-  ])
-  
-  .then((response) =>{
-    console.log(response)
-    if(response.whatToDoList==='View All Employees'){
-      console.log("it works");
-      //something about sql table
-    }
-     
-
-},
-
-function createDepartment (){
-
-    // inquirer.prompt for department info
-    inquirer.prompt([
-
+function whatToDoQestion() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do? ",
+        name: "whatToDoList",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+        ],
+      },
     ])
     .then((response) => {
-  
-      //THEN INSERT INTO department () values
-      db.query(`INSERT INTO department (name) VALUES (?)`, response.name, function(err, response){
+      console.log(response);
+      if (response.whatToDoList === "View All Employees") {
+        
+          db.query('SELECT * FROM employee',
 
-          askUserForAction();
+     function(err, results) {
+     console.log(results);
       });
+        console.log("it works");
+      }
     });
-    
-},
+}
 
-function createRole(){
-  // Get all existing departments
-  db.query(`SELECT`, response.name, function(err,response){
-    inquirer.promt ([
-
-    ])
-    .then((response) => {
-
-      // THEN INSERT INTO department () VALUES
-      db.query(`INSERT INTO roles (name) VALUES (?)`, response.name), function(err, response){
-
+function createDepartment() {
+  // inquirer.prompt for department info
+  inquirer.prompt([]).then((response) => {
+    //THEN INSERT INTO department () values
+    db.query(
+      `INSERT INTO department (name) VALUES (?)`,
+      response.name,
+      function (err, response) {
         askUserForAction();
       }
-    })
-  })
-})}
+    );
+  });
+}
+
+function createRole() {
+  // Get all existing departments
+  db.query(`SELECT`, response.name, function (err, response) {
+    inquirer.promt([]);
+  }).then((response) => {
+    // THEN INSERT INTO department () VALUES
+    db.query(`INSERT INTO roles (name) VALUES (?)`, response.name),
+      function (err, response) {
+        askUserForAction();
+      };
+  });
+}
+
 whatToDoQestion();
